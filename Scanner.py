4f523cde41720ace1c7250af3ae4fd483ada3ced
Gilad Cohen <gilad4591@gmail.com>
2021-02-01 09:54:20 +0200

    remove unnecessary parts and add comments

--- Scanner.py
+++ Scanner.py
@@ -60,7 +60,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
 
-blurred=cv2.GaussianBlur(gray, (5, 5), 1)
+blurred=cv2.GaussianBlur(gray, (7, 7), 2)
 threshold = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 threshold = cv2.fastNlMeansDenoising(threshold, 11, 31, 9)
 cv2.imshow("Blur", blurred)
